7050bfe8a7a5cabb5c3d538631f711b7d8cd50a3
Tyler Long <tyler4long@gmail.com>
2018-04-12 15:30:29 +0800

    Display WebHook URI

--- src/web/index.js
+++ src/web/index.js
@@ -1,7 +1,6 @@
 import './index.css'
 
-console.log('Hello world')
-
 global.startService(webhookUri => {
-  console.log(webhookUri)
+  console.log('WebHook URI is', webhookUri)
+  document.getElementsByTagName('h1')[0].innerText = webhookUri
 })
