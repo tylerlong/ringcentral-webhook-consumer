93514db84beb2a292370a46424ce730871d71c40
Tyler Long <tyler4long@gmail.com>
2018-04-12 16:14:29 +0800

    First fully working version

--- src/electron/service.js
+++ src/electron/service.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import detectPort from 'detect-port'
+import bodyParser from 'body-parser'
 
 import { startNgrok } from './ngrok'
 
@@ -9,7 +10,14 @@ export const startService = async (webhookUriCallback) => {
     const webhookUri = `${publicUrl}/webhook`
     webhookUriCallback(webhookUri)
     const app = express()
+    app.use(bodyParser.json())
     app.get('/webhook', async (req, res) => {
+      console.log('WebHook calls me!')
+      res.send('response from webhook')
+    })
+    app.post('/webhook', async (req, res) => {
+      console.log(req)
+      res.header('validation-token', req.header('validation-token'))
       res.send('response from webhook')
     })
     app.listen(port)
