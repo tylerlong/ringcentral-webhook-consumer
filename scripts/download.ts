e33046798039431dbe02bbe09266ed5883cc54d2
Tyler Liu <tyler4long@gmail.com>
2024-06-09 20:39:12 -0700

    Update download script

--- scripts/download.ts
+++ scripts/download.ts
@@ -3,7 +3,7 @@ import { run } from 'shell-commands';
 const main = async () => {
   await run(`
 rm -rf ngrok.zip ngrok
-curl -o ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
+curl -o ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-darwin-${process.arch === 'arm64' ? 'arm64' : 'amd64'}.zip
 unzip ngrok.zip
 rm ngrok.zip
 chmod a+x ngrok
